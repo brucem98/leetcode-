a0756a8c8bdf7e262bdb2ad76b2c43389da13aa6
bruce_milliken <brucemilliken98@gmail.com>
2022-05-12 19:23:10 +0100

    Add some code

--- Algorithm I/Day 1 Binary Search/704_binary_Search.js
+++ Algorithm I/Day 1 Binary Search/704_binary_Search.js
@@ -34,3 +34,22 @@ function search(nums, target) {
 };
 
 console.log(search([1,3,4,5,7,9,10],7))
+
+function find(isBadVersion) {
+    // @param {integer} n Total versions
+    // @return {integer} The first bad version
+    
+    return function(n) {
+        // Min left start is 1
+        let left = 1, right = n
+        while(left < right) {
+            const mid = left + Math.floor((right-left)/2)
+            if(isBadVersion(mid)) {
+                right = mid
+            } else {
+                left = mid + 1
+            }
+        }
+        return left
+    };
+};
